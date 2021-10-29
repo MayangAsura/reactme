aef4ba3b1300159e1d71579cccb9304eca0be56a
MayangAsura <mayang.asura123@gmail.com>
2021-10-29 15:21:11 +0700

    added penjelasan import index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'; 
+import ReactDOM from 'react-dom'; // fuction utk render ke browser
+import './index.css'; // utk styling
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
